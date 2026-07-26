1410b2347c26551ba8d7b1d82c8b34847f5565e4
Samuel Grund Sørensen <samuel_grund@hotmail.com>
2026-07-26 15:03:55 +0200

    Replace Google Chrome with Firefox in dock script

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
